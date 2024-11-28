ce278ad5a90daddfc03a910e5f4e6120771eabb7
Aditya <adityagundg14@gmail.com>
2024-11-28 20:28:41 +0530

    Intial Setup

---
+++ user-auth/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/user-auth/user-auth.component';
---
+++ user-auth/src/lib/user-auth/user-auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-user-auth',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-auth.component.html',
+  styleUrl: './user-auth.component.css',
+})
+export class UserAuthComponent {}
